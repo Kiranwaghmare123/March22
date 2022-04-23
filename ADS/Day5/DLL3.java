375ab1901edcae182b1debb2237fcbaa72b67b17
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2022-04-23 18:01:27 +0530

    Update DLL3.java

--- ADS/Day5/DLL3.java
+++ ADS/Day5/DLL3.java
@@ -80,6 +80,28 @@ void append(int new_data)
 	
 
 }	
+
+void deleteNode(Node del)
+		{
+
+			// Base case
+			if (head == null || del == null) {
+				return;
+			}
+
+			if (head == del) {
+				head = del.next;
+			}
+			if (del.next != null) {
+				del.next.prev = del.prev;
+			}
+			if (del.prev != null) {
+				del.prev.next = del.next;
+			}
+			return;
+		}
+		
+	
 public static void main(String args[])
 {	
 	DLL2 d1 = new DLL2();
